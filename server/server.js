2cc424835b4077c2234e8c8a8a76ce85630be292
amps1819 <47330054+amps1819@users.noreply.github.com>
2024-01-04 01:38:09 +0000

    Preparando el repositorio para alojarlo en la nube Netlify+MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const db = require("./app/models");
 const Role = db.role;
 
 db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+  .connect(`${dbConfig.HOST}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
